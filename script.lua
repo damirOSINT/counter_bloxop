3e749458cd96e8b8e84cc91663caf5f8afb9ecc4
damirOSINT <damirlukash17@gmail.com>
2025-03-01 18:37:13 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -4,7 +4,7 @@ local Dot = Instance.new("Frame")
 ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
 Dot.Size = UDim2.new(0, 5, 0, 5)  -- Размер точки 5x5 пикселей
-Dot.Position = UDim2.new(0.5, -2, 0.5, -6)
+Dot.Position = UDim2.new(0.5, -2, 0.48, 0)  -- Корректируем центр вручную
 Dot.BackgroundColor3 = Color3.fromRGB(0, 255, 0)  -- Зеленый цвет
 Dot.BorderSizePixel = 0  -- Без границы
 Dot.Parent = ScreenGui
